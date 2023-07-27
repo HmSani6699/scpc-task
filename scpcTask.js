527e73b85d401ba249bfcf2e7babae852df1911c
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-07-27 22:40:42 +0600

    added the four task

--- scpcTask.js
+++ scpcTask.js
@@ -5,13 +5,14 @@ function taskOne(text){
     return text.split("").reverse().join('')
 }
 
-console.log(taskOne('Alhamdolillha')); 
+// const result =taskOne('Alhamdolillha')
+// console.log(result); 
 
 
 /*/========================//
          Task Two
 //=========================/*/
-function taskOne(arr) {
+function taskTwo(arr) {
     let result = 0
     for(num in arr){
         if(arr[num]>0){
@@ -20,4 +21,37 @@ function taskOne(arr) {
     }
     return result
 }
-console.log(taskOne([2, -5, 10, -3, 7])); 
+
+// const result =taskTwo([2, -5, 10, -3, 7])
+// console.log(result); 
+
+
+/*/========================//
+         Task Three
+//=========================/*/
+
+function taskThree(arr) {
+    var mf = 1;
+    var m = 0;
+    var item;
+
+    for (a in arr) {
+        for (b in arr) {
+            if (arr[a] == arr[b]) m++;
+            if (mf < m) {
+                mf = m;
+                item = arr[a];
+            }
+        }
+
+        m = 0;
+    }
+    return item
+}
+// const result = taskThree([3, 5, 2,5, 3, 3, 1, 4, 5]);
+// console.log(result); 
+
+
+/*/========================//
+         Task Four
+//=========================/*/
