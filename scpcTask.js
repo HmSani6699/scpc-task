21dfc0cbf7f8c61ec2f700a66ecc4a2e1fb13217
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-07-27 20:08:06 +0600

    complete the tow task

--- scpcTask.js
+++ scpcTask.js
@@ -6,3 +6,18 @@ function taskOne(text){
 }
 
 console.log(taskOne('Alhamdolillha')); 
+
+
+/*/========================//
+         Task Two
+//=========================/*/
+function taskOne(arr) {
+    let result = 0
+    for(num in arr){
+        if(arr[num]>0){
+            result+=arr[num]
+        }
+    }
+    return result
+}
+console.log(taskOne([2, -5, 10, -3, 7])); 
